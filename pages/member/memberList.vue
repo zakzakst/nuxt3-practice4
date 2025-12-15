07a9b5ddb18fe0a8d75a7b318fb55094b3aa5a9f
zakzakst <e1433104@gmail.com>
2025-12-15 11:39:24 +0900

    3-5

--- pages/member/memberList.vue
+++ pages/member/memberList.vue
@@ -1,39 +1,45 @@
 <script setup lang="ts">
-import { useState } from "nuxt/app";
+import { useState, useHead } from "nuxt/app";
 import type { Member } from "../../interfaces";
 const memberList = useState<Map<number, Member>>("memberList");
+definePageMeta({
+  layout: "member",
+});
+const PAGE_TITLE = "会員リスト";
+useHead({
+  title: PAGE_TITLE,
+});
 </script>
 
 <template>
-  <h1>会員管理</h1>
   <nav>
     <ul>
       <li><NuxtLink :to="{ name: 'index' }">TOP</NuxtLink></li>
-      <li>会員リスト</li>
+      <li>{{ PAGE_TITLE }}</li>
     </ul>
   </nav>
   <section>
-    <h2>会員リスト</h2>
+    <h2>{{ PAGE_TITLE }}</h2>
     <p>
-      <!-- 新規登録は<NuxtLink :to="{ name: 'member-memberAdd' }">こちら</NuxtLink
-      >から -->
-      新規登録は<NuxtLink :to="{ name: 'member-memberList-memberAdd' }"
-        >こちら</NuxtLink
+      新規登録は<NuxtLink :to="{ name: 'member-memberAdd' }">こちら</NuxtLink
       >から
+      <!-- 新規登録は<NuxtLink :to="{ name: 'member-memberList-memberAdd' }"
+        >こちら</NuxtLink
+      >から -->
     </p>
     <section>
       <ul>
         <li v-for="[id, member] in memberList" :key="id">
-          <!-- <NuxtLink :to="{ name: 'member-memberDetail-id', params: { id } }"
-            >IDが{{ id }}の{{ member.name }}さん</NuxtLink
-          > -->
-          <NuxtLink
-            :to="{ name: 'member-memberList-memberDetail-id', params: { id } }"
+          <NuxtLink :to="{ name: 'member-memberDetail-id', params: { id } }"
             >IDが{{ id }}の{{ member.name }}さん</NuxtLink
           >
+          <!-- <NuxtLink
+            :to="{ name: 'member-memberList-memberDetail-id', params: { id } }"
+            >IDが{{ id }}の{{ member.name }}さん</NuxtLink
+          > -->
         </li>
       </ul>
     </section>
-    <NuxtPage />
+    <!-- <NuxtPage /> -->
   </section>
 </template>
