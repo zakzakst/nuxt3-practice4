61b90c61065c581ee4018b79994a3a18a1120edb
zakzakst <e1433104@gmail.com>
2025-12-15 13:31:05 +0900

    4-1 todo

--- app.vue
+++ app.vue
@@ -1,30 +1,5 @@
-<script setup lang="ts">
-import { useState } from "nuxt/app";
-import type { City } from "./interfaces";
-
-useState<Map<number, City>>("cityList", (): Map<number, City> => {
-  const cityListInit = new Map<number, City>();
-  cityListInit.set(1853909, {
-    id: 1853909,
-    name: "大阪",
-    q: "Osaka",
-  });
-  cityListInit.set(1859171, {
-    id: 1859171,
-    name: "神戸",
-    q: "Kobe",
-  });
-  cityListInit.set(1862627, {
-    id: 1862627,
-    name: "姫路",
-    q: "Himeji",
-  });
-  return cityListInit;
-});
-</script>
-
 <template>
-  <header>fetchサンプル</header>
+  <header>todoサンプル</header>
   <main>
     <NuxtPage />
   </main>
--- interfaces.ts
+++ interfaces.ts
@@ -11,3 +11,31 @@ export interface City {
   name: string;
   q: string;
 }
+
+interface UserAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+interface UserCompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: UserAddress;
+  phone: string;
+  website: string;
+  company: UserCompany;
+}
