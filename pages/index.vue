d26ada305f1ebfe22472e29951426577636c99f6
zakzakst <e1433104@gmail.com>
2025-12-16 10:58:32 +0900

    7-2

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
 import TheLoggedInSection from "../components/TheLoggedInSection.vue";
+// import type { AuthUser } from "../interfaces";
+// import { navigateTo, useCookie } from "nuxt/app";
+
+// const loginTokenCookie = useCookie<string | null>("loginToken");
+// const loginUserCookie = useCookie<AuthUser | null>("loginUser");
+// if (!loginTokenCookie.value || !loginUserCookie.value) {
+//   await navigateTo("/login");
+// } else {
+//   console.log(loginTokenCookie.value, loginUserCookie.value, "test");
+// }
+
+definePageMeta({
+  // middleware: (to, from) => {
+  //   const loginTokenCookie = useCookie<string | null>("loginToken");
+  //   const loginUserCookie = useCookie<AuthUser | null>("loginUser");
+  //   if (!loginTokenCookie.value || !loginUserCookie.value) {
+  //     return navigateTo("/login");
+  //   } else {
+  //     return;
+  //   }
+  // },
+  middleware: [
+    // "logging",
+    "loggedin-check",
+  ],
+});
 </script>
 
 <template>
