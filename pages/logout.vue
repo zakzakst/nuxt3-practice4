09cf0ab351cc8e2fa27b69d0e62b93a5639462fe
zakzakst <e1433104@gmail.com>
2025-12-16 10:27:44 +0900

    7-1

---
+++ pages/logout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useCookie, navigateTo } from "nuxt/app";
+import type { AuthUser } from "../interfaces";
+
+const loginUserCookie = useCookie<AuthUser | null>("loginUser");
+loginUserCookie.value = null;
+const loginTokenCookie = useCookie<string | null>("loginToken");
+loginTokenCookie.value = null;
+await navigateTo("/");
+</script>
