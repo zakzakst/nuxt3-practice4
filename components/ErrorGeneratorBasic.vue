078ce0f76db532b2e03325748bb2d88cfc5137fd
zakzakst <e1433104@gmail.com>
2025-12-16 09:26:31 +0900

    6-1

---
+++ components/ErrorGeneratorBasic.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const onThrowsErrorClick = (): void => {
+  throw createError("疑似エラー発生。");
+};
+</script>
+
+<template>
+  <section>
+    <button v-on:click="onThrowsErrorClick">エラーを発生</button>
+  </section>
+</template>
