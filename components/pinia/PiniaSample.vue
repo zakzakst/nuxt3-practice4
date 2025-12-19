a9a38fb99423094a03dccdfc071866c0ef3c0fff
zakzakst <e1433104@gmail.com>
2025-12-19 11:50:26 +0900

    fix

--- components/pinia/PiniaSample.vue
+++ components/pinia/PiniaSample.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 const counter = useCounterStore();
+const _counter = useCounter();
 </script>
 
 <template>
   <div>
     <p>数の表示</p>
     <p>count: {{ counter.count }}</p>
+    <p>_counter: {{ _counter.count }}</p>
   </div>
 </template>
--- pages/pinia/index.vue
+++ pages/pinia/index.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 // import PiniaSample from "@components/pinia/PiniaSample.vue";
+// import { useCounter } from "@/composables/useCounter";
 import PiniaSample from "../../components/pinia/PiniaSample.vue";
 
 const counter = useCounterStore();
+const _counter = useCounter();
 </script>
 
 <template>
   <div>
+    <p>pinia</p>
     <button @click="counter.increment">+</button>
     <button @click="counter.decrement">-</button>
+  </div>
+  <div>
+    <p>composables</p>
+    <p>{{ _counter.count }}</p>
+    <button @click="_counter.increment">+</button>
+    <button @click="_counter.decrement">-</button>
   </div>
   <PiniaSample />
 </template>
