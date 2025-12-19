ae708b1bbc51917493c59fb162448f9040b7a73c
zakzakst <e1433104@gmail.com>
2025-12-19 14:21:28 +0900

    fix

---
+++ pages/form/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useForm, useField } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import { formSchema } from "../../schemas/form";
+
+const { handleSubmit } = useForm({
+  validationSchema: toTypedSchema(formSchema),
+});
+
+const { value: email, errorMessage: emailError } = useField("email");
+const { value: password, errorMessage: passwordError } = useField("password");
+
+const onSubmit = handleSubmit((values) => {
+  console.log(values);
+});
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <input v-model="email" />
+    <p class="text-red-500">{{ emailError }}</p>
+
+    <input type="password" v-model="password" />
+    <p class="text-red-500">{{ passwordError }}</p>
+
+    <button>送信</button>
+  </form>
+</template>
